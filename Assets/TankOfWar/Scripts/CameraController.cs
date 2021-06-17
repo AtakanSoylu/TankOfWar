4a2c9f66c85aeba1bffb2627f7c06652528595e8
AtakanSoylu <59143350+AtakanSoylu@users.noreply.github.com>
2021-06-17 13:57:05 +0300

    +added : basic movement for camera

---
+++ Assets/TankOfWar/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TankOfWar
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private CameraSettings _cameraSettings;
+        [SerializeField] private Transform _targetTranform;
+        [SerializeField] private Transform _cameraTranform;
+
+        private void Update()
+        {
+            CameraRotationFollow();
+            CameraMovementFollow();
+        }
+
+        private void CameraRotationFollow()
+        {
+            _cameraTranform.rotation = Quaternion.Lerp(_cameraTranform.rotation,
+                Quaternion.LookRotation(_targetTranform.position - _cameraTranform.position),
+                Time.deltaTime * _cameraSettings.RotationLerpSpeed);
+        }
+
+        private void CameraMovementFollow()
+        {
+            _cameraTranform.position = Vector3.Lerp(_cameraTranform.position,
+                _targetTranform.position + _cameraSettings.PositionOffset,
+                Time.deltaTime * _cameraSettings.PositionLerp);
+        }
+
+
+
+
+
+    }
+}
---
+++ Assets/TankOfWar/Scripts/CameraSettings.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TankOfWar
+{
+    [CreateAssetMenu(menuName = "TankOfWar/Camera/Camera Settings")]
+    public class CameraSettings : ScriptableObject
+    {
+        [SerializeField] private float _rotationLerpSpeed = 1;
+        public float RotationLerpSpeed { get { return _rotationLerpSpeed; } }
+
+        [SerializeField] private Vector3 _positionOffset ;
+        public Vector3 PositionOffset { get { return _positionOffset; } }
+
+        [SerializeField] private float _positionLerp = 1;
+        public float PositionLerp { get { return _positionLerp; } }
+
+    }
+}
