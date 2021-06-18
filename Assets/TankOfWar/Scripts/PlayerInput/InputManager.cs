f9f6cfd58c67d2a523924520b6f1c1921d1f931b
AtakanSoylu <59143350+AtakanSoylu@users.noreply.github.com>
2021-06-18 21:33:03 +0300

    +added : rotation input data

--- Assets/TankOfWar/Scripts/PlayerInput/InputManager.cs
+++ Assets/TankOfWar/Scripts/PlayerInput/InputManager.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace TankOfWar.PlayerInput
-
 {
     public class InputManager : MonoBehaviour
     {
         [SerializeField] private InputData _inputData;
-
+        [SerializeField] private InputData _rotationInputData;
+        private Vector3 _lastMouseInput;
         private void Update()
         {
             _inputData.Horizontal = Input.GetAxis("Horizontal");
             _inputData.Vertical = Input.GetAxis("Vertical");
+
+            _rotationInputData.Horizontal = (Input.mousePosition.x - _lastMouseInput.x);
+            _lastMouseInput = Input.mousePosition;
         }
     }
 }
