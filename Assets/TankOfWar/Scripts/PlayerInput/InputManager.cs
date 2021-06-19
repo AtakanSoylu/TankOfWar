703f60a6da644728aa56a845be2c27b56ae515e4
AtakanSoylu <59143350+AtakanSoylu@users.noreply.github.com>
2021-06-19 10:07:28 +0300

    +refactor : input system

--- Assets/TankOfWar/Scripts/PlayerInput/InputManager.cs
+++ Assets/TankOfWar/Scripts/PlayerInput/InputManager.cs
@@ -6,16 +6,15 @@ namespace TankOfWar.PlayerInput
 {
     public class InputManager : MonoBehaviour
     {
-        [SerializeField] private InputData _inputData;
-        [SerializeField] private InputData _rotationInputData;
-        private Vector3 _lastMouseInput;
+        [SerializeField] private InputData[] _inputDataArray;
         private void Update()
         {
-            _inputData.Horizontal = Input.GetAxis("Horizontal");
-            _inputData.Vertical = Input.GetAxis("Vertical");
+            for(int i = 0; i < _inputDataArray.Length; i++)
+            {
+                _inputDataArray[i].ProcessInput();
+            }
 
-            _rotationInputData.Horizontal = (Input.mousePosition.x - _lastMouseInput.x);
-            _lastMouseInput = Input.mousePosition;
+
         }
     }
 }
