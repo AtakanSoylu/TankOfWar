1df76adcd5e1d4e4aee50e2f3dcf0c0c1df4e46d
AtakanSoylu <59143350+AtakanSoylu@users.noreply.github.com>
2021-06-19 18:10:40 +0300

    +create : shoot manager

---
+++ Assets/TankOfWar/Scripts/Inventory/ScriptableShootManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace TankOfWar.Inventory
+{
+    [CreateAssetMenu(menuName = "TankOfWar/Inventory/ScriptableShootManager")]
+    public class ScriptableShootManager : AbstractScriptableManager<ScriptableShootManager>
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Debug.Log("Scriptable shoot manager active");
+        }
+        public override void Destroy()
+        {
+            base.Initialize();
+            Debug.Log("Scriptable shoot manager destroy");
+            
+        }
+
+    }
+}
